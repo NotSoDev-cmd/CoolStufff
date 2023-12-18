e10800a87f671179526329b811d3ed5ca1828c2d
NotSoDev <68327824+NotSoDev-cmd@users.noreply.github.com>
2023-12-18 21:37:24 +0530

    chupa chups

--- src/locate.js
+++ src/locate.js
@@ -3,15 +3,34 @@ import logo from './logo.jpg';
 import './For1.css';
 import { Link } from 'react-router-dom';
 
+import './Jim.css';
 
-import './App.css';
+
 
 function Locate() {
     return (
         <div className="App">
       <header className="App-header1">
-        
-      <p>Adding Sign up function left.</p>
+        <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+        <link rel="stylesheet" href="bootstrap/css/font-awesome.min.css"></link>
+        </head>
+        <div class="form-div">
+        <img src={logo} alt='NerdWeb' height={200} width={200}></img>
+		      <h2>Login Here</h2>
+		      <form>
+			      <input type="text" placeholder="User Name"></input>
+			      <br></br>
+			      <input type="password" placeholder="Password"></input>
+			      <br></br>
+			      <input type="submit" value="Login"></input>
+          </form>
+        </div>
+		    
+		    
+      
+      
+
 
       </header>
     </div>
